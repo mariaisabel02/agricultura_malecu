5bf1f6018202c87305f3bf19ce533dbbf653852c
isa <maria.murilloquintana@ucr.ac.cr>
2020-01-09 22:25:53 -0600

    mitad de vocabulario 2

--- app/src/main/java/ucr/ecci/agriculturamalecu/vocabulario.java
+++ app/src/main/java/ucr/ecci/agriculturamalecu/vocabulario.java
@@ -2,7 +2,11 @@ package ucr.ecci.agriculturamalecu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class vocabulario extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class vocabulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vocabulario);
+        ImageButton boton_principal = (ImageButton)findViewById(R.id.btn_principal);
+        ImageButton boton_adelante = (ImageButton)findViewById(R.id.btn_adelante);
+        final Context context = this;
+
+        boton_adelante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, vocabulario2.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_der, R.anim.slide_out_izq);
+            }
+        });
+
+        boton_principal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, pagina_principal.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.izq_der, R.anim.der_izq);
+            }
+        });
     }
 }
---
+++ app/src/main/java/ucr/ecci/agriculturamalecu/vocabulario2.java
@@ -0,0 +1,14 @@
+package ucr.ecci.agriculturamalecu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class vocabulario2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vocabulario2);
+    }
+}
