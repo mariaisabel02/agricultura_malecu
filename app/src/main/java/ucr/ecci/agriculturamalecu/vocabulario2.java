4deaf7d5ef3c6700335f867dd60722f3515e55f5
isa <maria.murilloquintana@ucr.ac.cr>
2020-01-15 21:57:23 -0600

    inicio de vocabulario 3

--- app/src/main/java/ucr/ecci/agriculturamalecu/vocabulario2.java
+++ app/src/main/java/ucr/ecci/agriculturamalecu/vocabulario2.java
@@ -2,7 +2,11 @@ package ucr.ecci.agriculturamalecu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class vocabulario2 extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class vocabulario2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vocabulario2);
+        ImageButton boton_principal = (ImageButton)findViewById(R.id.btn_principal);
+        ImageButton boton_adelante = (ImageButton)findViewById(R.id.btn_adelante);
+        ImageButton boton_atras = (ImageButton)findViewById(R.id.btn_atras);
+        final Context context = this;
+
+        boton_adelante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, vocabulario3.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_der, R.anim.slide_out_izq);
+            }
+        });
+
+        boton_principal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, pagina_principal.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.izq_der, R.anim.der_izq);
+            }
+        });
     }
+
 }
