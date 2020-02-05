3433eb2deccbeaffa017fd8efce2ab9b87c7881f
isa <maria.murilloquintana@ucr.ac.cr>
2020-02-04 22:17:32 -0600

    corregido ainh

--- app/src/main/java/ucr/ecci/agriculturamalecu/ainh.java
+++ app/src/main/java/ucr/ecci/agriculturamalecu/ainh.java
@@ -14,12 +14,10 @@ public class ainh extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ainh);
-
-        ImageButton boton_atras = (ImageButton)findViewById(R.id.btn_atras);
+        ImageButton boton_principal = (ImageButton)findViewById(R.id.btn_principal);
         final Context context = this;
 
-
-        boton_atras.setOnClickListener(new View.OnClickListener() {
+        boton_principal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(context, agricultura_tradicional.class);
